18df87d52d317c8f80c6bb63d77a6ce7953c070c
Aaron Block <ablock@austincollege.edu>
2019-02-04 14:25:28 -0600

    Removed autogenerated Garbage and changed background color

--- SampleSpriteKit/GameViewController.swift
+++ SampleSpriteKit/GameViewController.swift
@@ -16,14 +16,10 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
         
         if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
+            let scene = SKScene(size: view.bounds.size)
+            scene.scaleMode = .aspectFill
+            scene.backgroundColor = UIColor.red
+            view.presentScene(scene)
             
             view.ignoresSiblingOrder = true
             
